c8154f244a0bfb39ed28a8165e879bd7b2270ba5
anish510 <anishjoshi510@gmail.com>
2023-06-09 15:10:00 +0545

    static link

--- count/urls.py
+++ count/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('counter',views.counter, name = 'counter'),
-    
+    path('', views.index, name='index'),
+    path('second_index/', views.second_index, name='second_index'),
+    path('counter', views.counter, name='counter'),
+
 ]
--- count/views.py
+++ count/views.py
@@ -3,10 +3,16 @@ from django.http import HttpRequest
 
 # Create your views here.
 
+
 def index(request):
     return render(request, 'index.html')
+
+
+def second_index(request):
+    return render(request, "second_index.html")
+
 
 def counter(request):
     texts = request.POST['texts']
     total_count = len(texts.split())
-    return render (request, 'counter.html',{'words': total_count})
+    return render(request, 'counter.html', {'words': total_count})
