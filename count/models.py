0d6968414c6ab14dd8d9ccc145876c761a56c5c6
anish510 <anishjoshi510@gmail.com>
2023-06-10 21:56:39 +0545

    models

--- count/models.py
+++ count/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 
-class features:
-    id: int
-    name: str
-    details: str
+class features(models.Model):
+    name = models.CharField(max_length=100)
+    details = models.CharField(max_length=500)
